a6247b566f29ab22a0ef8a82e4d7e028f0e7416e
AlexRothebest <alex_rozhkov@ukr.net>
2021-06-07 23:39:25 +0300

    Another fucking attempt

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,4 +4,4 @@ git remote add production ssh://root@$SERVER_IP$SERVER_PROJECT_DIRECTORY
 
 git push --force production master
 
-# ssh root@$SERVER_IP "cd $SERVER_PROJECT_DIRECTORY && npm install && systemctl restart gitlam_git.service && exit"
+ssh root@$SERVER_IP "cd $SERVER_PROJECT_DIRECTORY && npm install && systemctl restart gitlam_git.service && exit"
